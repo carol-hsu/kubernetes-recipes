0a3d8490c2ccd1d47a05c9e1539fb32dbc669a60
nosus <carol_hsu@trend.com.tw>
2016-03-17 02:09:23 +0800

    simplify master run

--- kubernetes/recipes/kubernetes-master-run.rb
+++ kubernetes/recipes/kubernetes-master-run.rb
@@ -1,24 +1,8 @@
 service "flanneld" do
 	action :start
-	notifies :run, 'bash[wait_flanneld]', :delayed
-end
-
-bash 'wait_flanneld' do
-	user 'root'
-	cwd '/tmp'
-	code <<-EOH
-	tries=0
-        while [ ! -f /run/flannel/subnet.env -a $tries -lt 10 ]; do
-            sleep 1
-            tries=$((tries + 1))
-        done
-	EOH
-
-	action :nothing
-	notifies :start, 'service[kubernetes-master]', :delayed
 end
 
 service "kubernetes-master" do
-	action :nothing
+	action :start
 end
 
